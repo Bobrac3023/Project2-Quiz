de01760b9ce93f95d371f778297b21454c4e0791
Roshan Sequeira <roshan@secureworkspace.tech>
2024-07-18 06:07:07 +0000

    create variables and function array of questions in script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,102 @@
-function buildQuiz(){}
 
-function showResults(){}
+const quizH2Element = document.getElementById ("quiz");
+const answerButton = document.getElementById ("answer-buttons");
+const nextButton = document.getElementById ("next-btn");
 
-// display quiz right away
-buildQuiz();
+const questions = [
+    {
+    question: "Who is the President of India",
+    answer: [{
+            text: "Smt Sonia Gandhi",
+            correct: false
+        },
+        {
+            text: "Smt Droupadi Murmu",
+            correct: true
+        },
+        {
+            text: "Smt Sarojini Naidu",
+            correct: false
+        },
+        {
+            text: " Smt Pratiba Patil ",
+            correct: false
+        },
 
-// on submit, show results
-submitButton.addEventListener('click', showResults);
+    ]
+},
+{
+    question: "Who is first prime minister of Independent India",
+    answer: [{
+            text: "Smt Indira Gandhi",
+            correct: false
+        },
+        {
+            text: "Shri Vallabhai Patel",
+            correct: false
+        },
+        {
+            text: "Shri Pandit Jawaharlal Nehru",
+            correct: true
+        },
+        {
+            text: " Shri Deve Gowda ",
+            correct: false
+        },
+
+{
+    question: "Who is last Governor-General of Dominion of India",
+    answer: [{
+            text: " Shri Chakravarti Rajagopalachari ",
+            correct: true
+        },
+        {
+            text: "Lord William Bentinck",
+            correct: false
+        },
+        {
+            text: "Shri Rajendra Prasad",
+            correct: false
+        },
+        {
+            text: " Dr Maulana Azad ",
+            correct: false
+        },
+        {
+    question: "Who is first education minister of Idependent India",
+            answer: [{
+                    text: "Dr John Matthai",
+                    correct: false
+                },
+                {
+                    text: "Shri Vallabhai Patel",
+                    correct: false
+                },
+                {
+                    text: "Dr Babasaheb Ambedkar",
+                    correct: false
+                },
+                {
+                    text: " Dr Maulana Azad",
+                    correct: true
+                },
+                {
+    question: "Who is first home minister of Idependent India",
+                    answer: [{
+                            text: "Shri Jagjivan Ram ",
+                            correct: false
+                        },
+                        {
+                            text: "Shri Vallabhai Patel",
+                            correct: true
+                        },
+                        {
+                            text: "Shri Pandit Jawaharlal Nehru",
+                            correct: false
+                        },
+                        {
+                            text: " Sardar Baldev Singh ",
+                            correct: false
+                        },
+                                
+]
