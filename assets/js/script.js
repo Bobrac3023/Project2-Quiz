282e54edeb9538f099bd216847160371e9984ce0
Roshan Sequeria <roshan@secureworkspace.tech>
2024-07-22 13:00:17 +0000

    write array constructor to disable mouse click once the user selects an answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-const  totalQuestions = [{
+const totalQuestions = [{
         question: "Who is the President of India",
         choices: [{
                 text: "Smt Sonia Gandhi",
-                result : false
+                result: false
             },
             {
                 text: "Smt Droupadi Murmu",
@@ -22,7 +22,7 @@ const  totalQuestions = [{
         question: "Who is last Governor-General of Dominion of India",
         choices: [{
                 text: " Shri Chakravarti Rajagopalachari ",
-                result : true
+                result: true
             },
             {
                 text: "Lord William Bentinck",
@@ -41,31 +41,31 @@ const  totalQuestions = [{
 
 ];
 
-/* variables */
+
+// variables 
 const quizH2Element = document.getElementById("quiz");
 const answerButton = document.getElementById("answer-buttons");
 const submitButton = document.getElementById("submit-button");
-const scoreArea =document.getElementById("score-area");
+//const scoreArea = document.getElementById("score-area");
 
-/* variable to store question index and score */
+//variable to store question index and score 
 let presentQuestionIndex = 0;
 let score = 0;
-let correctAnswers=0;
-let wrongAnswers =0;
 
-/* function to start quiz*/
+
+// function to start quiz
 function initiateQuiz() {
-    /* reset question index to zero*/
+    //reset question index to zero
     presentQuestionIndex = 0;
-    /* reset score index to zero*/
+    //reset score index to zero
     score = 0;
-    correctAnswers=0;
-    wrongAnswers=0;
+    correctAnswers = 0;
+    wrongAnswers = 0;
     submitButton.innerHTML = "Submit"
     displayQuestion();
 }
 
-/* function to display questions*/
+//function to display questions
 function displayQuestion() {
     resetState();
     /* var to get questions with index 0*/
@@ -75,68 +75,69 @@ function displayQuestion() {
     /* display question */
     quizH2Element.innerHTML = questionNo + "." + questionBank.question;
 
-    /* display answers with 'forEach' loop method and arrow function*/
+    // display answers with 'forEach' loop method and arrow function
     questionBank.choices.forEach(choices => {
-        /* Create 'Button' element */
+        // Create 'Button' element 
         const button = document.createElement('button');
-        /* add details to innerHTML of button Element*/
+        //insert details to innerHTML of button Element from array and object literals
         button.innerHTML = choices.text;
         button.classList.add('btn');
-        /* append button element as child inside the Div*/
+        // append button element as child inside the Div
         answerButton.appendChild(button);
-        /* Create ifLoop when user selects an option*/
+        //Create ifLoop when user selects an option
         if (choices.result) {
             button.dataset.result = choices.result;
         }
         button.addEventListener("click", selectChoice)
     });
 }
-/* function to remove buttons*/
-function resetState() {
-    while (answerButton.firstChild) {
-        answerButton.removeChild(answerButton.firstChild)
-        
-    }
-}
-/* function when user clicks on answers*/
+
 function selectChoice(e) {
     let selectedBtn = e.target;
     let isCorrect = selectedBtn.dataset.result === "true";
-    //let isWrong = selectedBtn.dataset.wrong === "true";
     if (isCorrect) {
         selectedBtn.classList.add("correct");
         score++;
-        correctAnswers++;
-        //alert("Great- This is the right answer!!!!");
+        alert("!!!!Great- This is the right answer!!!!");
     } else {
         selectedBtn.classList.add("incorrect");
-        //alert("Sorry- Incorrect- Try again!!!!");
-        wrongAnswers++;
+        alert(" !!!! Sorry- Incorrect Answer!!!!");
     }
+    Array.from(answerButton.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add ("correct");
+        }
+        button.disabled =true;
+    });
+    submitButton.style.display ="block";
 }
 
+// function to remove buttons
+function resetState() {
+    while (answerButton.firstChild) {
+        answerButton.removeChild(answerButton.firstChild)
+    }
+}
+//function when user clicks on answers
 function showscore() {
     resetState();
     quizH2Element.innerHTML = `You scored ${score} out of ${totalQuestions.length}!`;
-    scoreArea.innerHTML =`You got ${wrongAnswers} Wrong Answer(s),  you got ${correctAnswers}Correct Answer(s) `;
     submitButton.innerHTML = "Play-Again";
     submitButton.style.display = "block";
 }
 
-/* Create function for Submit Button */
+// function for Submit Button 
 function handleSubmitButton() {
     presentQuestionIndex++;
     if (presentQuestionIndex < totalQuestions.length) {
         displayQuestion();
     } else {
         showscore();
-       // correctAnswers;
-       // wrongAnswers;
     }
 
 }
 
-/* Create eventListener when user clicks submit button*/
+//eventListener when user clicks submit button
 submitButton.addEventListener("click", () => {
     if (presentQuestionIndex <= totalQuestions.length) {
         handleSubmitButton()
