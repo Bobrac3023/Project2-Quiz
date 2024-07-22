f1d099bf9daa40b3ec87e9bcfe3dacde8dd93f8f
Roshan Sequeria <roshan@secureworkspace.tech>
2024-07-22 07:21:18 +0000

    create submit button function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,127 +1,150 @@
-const questions = [{
-    question: "Who is the President of India",
-    choices: [{
-            text: "Smt Sonia Gandhi",
-            correct: false
-        },
-        {
-            text: "Smt Droupadi Murmu",
-            correct: true
-        },
-        {
-            text: "Smt Sarojini Naidu",
-            correct: false
-        },
-        {
-            text: " Smt Pratiba Patil ",
-            correct: false
-        }
-    ]
-},
-{
-    question: "Who is last Governor-General of Dominion of India",
-    choices: [{
-            text: " Shri Chakravarti Rajagopalachari ",
-            correct: true
-        },
-        {
-            text: "Lord William Bentinck",
-            correct: false
-        },
-        {
-            text: "Shri Rajendra Prasad",
-            correct: false
-        },
-        {
-            text: " Dr Maulana Azad ",
-            correct: false
-        },
-    ]
-}
+const  totalQuestions = [{
+        question: "Who is the President of India",
+        choices: [{
+                text: "Smt Sonia Gandhi",
+                result : false
+            },
+            {
+                text: "Smt Droupadi Murmu",
+                result: true
+            },
+            {
+                text: "Smt Sarojini Naidu",
+                result: false
+            },
+            {
+                text: " Smt Pratiba Patil ",
+                result: false
+            }
+        ]
+    },
+    {
+        question: "Who is last Governor-General of Dominion of India",
+        choices: [{
+                text: " Shri Chakravarti Rajagopalachari ",
+                result : true
+            },
+            {
+                text: "Lord William Bentinck",
+                result: false
+            },
+            {
+                text: "Shri Rajendra Prasad",
+                result: false
+            },
+            {
+                text: " Dr Maulana Azad ",
+                result: false
+            },
+        ]
+    }
 
 ];
-/* define variables */
+
+/* variables */
 const quizH2Element = document.getElementById("quiz");
 const answerButton = document.getElementById("answer-buttons");
 const submitButton = document.getElementById("submit-button");
+const scoreArea =document.getElementById("score-area");
 
-/* creat variable to store question index and score */
+/* variable to store question index and score */
 let presentQuestionIndex = 0;
 let score = 0;
+let correctAnswers=0;
+let wrongAnswers =0;
 
-/* write function to start quiz*/
+/* function to start quiz*/
 function initiateQuiz() {
-/* reset question index to zero*/
-presentQuestionIndex = 0;
-/* reset score index to zero*/
-score = 0;
-submitButton.innerHTML = "Submit"
-displayQuestion();
+    /* reset question index to zero*/
+    presentQuestionIndex = 0;
+    /* reset score index to zero*/
+    score = 0;
+    correctAnswers=0;
+    wrongAnswers=0;
+    submitButton.innerHTML = "Submit"
+    displayQuestion();
 }
 
-/* write function to display questions*/
+/* function to display questions*/
 function displayQuestion() {
-resetState();
-/* define var to get questions with index 0*/
-let questionBank = questions[presentQuestionIndex];
-/* increment question by one*/
-let questionNo = presentQuestionIndex + 1;
-/* display question */
-quizH2Element.innerHTML = questionNo + "." + questionBank.question;
+    resetState();
+    /* var to get questions with index 0*/
+    let questionBank = totalQuestions[presentQuestionIndex];
+    /* increment question by one*/
+    let questionNo = presentQuestionIndex + 1;
+    /* display question */
+    quizH2Element.innerHTML = questionNo + "." + questionBank.question;
 
-/* write function to display answers with 'forEach' loop method*/
-questionBank.choices.forEach(choices => {
-    /* Create 'Button' element */
-    const button = document.createElement('button');
-    /* add details to innerHTML of button Element*/
-    button.innerHTML = choices.text;
-    button.classList.add('btn');
-    /* append button element as child inside the Div*/
-    answerButton.appendChild(button);
-    /* Create ifLoop when user selects an option*/
-    if (choices.correct) {
-        button.dataset.correct = choices.correct;
-    }
-    button.addEventListener("click", selectChoice);
-});
+    /* display answers with 'forEach' loop method and arrow function*/
+    questionBank.choices.forEach(choices => {
+        /* Create 'Button' element */
+        const button = document.createElement('button');
+        /* add details to innerHTML of button Element*/
+        button.innerHTML = choices.text;
+        button.classList.add('btn');
+        /* append button element as child inside the Div*/
+        answerButton.appendChild(button);
+        /* Create ifLoop when user selects an option*/
+        if (choices.result) {
+            button.dataset.result = choices.result;
+        }
+        button.addEventListener("click", selectChoice)
+    });
 }
-/* Create function to remove buttons*/
+/* function to remove buttons*/
 function resetState() {
-while (answerButton.firstChild) {
-    answerButton.removeChild(answerButton.firstChild)
+    while (answerButton.firstChild) {
+        answerButton.removeChild(answerButton.firstChild)
+        
+    }
 }
+/* function when user clicks on answers*/
+function selectChoice(e) {
+    let selectedBtn = e.target;
+    let isCorrect = selectedBtn.dataset.result === "true";
+    //let isWrong = selectedBtn.dataset.wrong === "true";
+    if (isCorrect) {
+        selectedBtn.classList.add("correct");
+        score++;
+        correctAnswers++;
+        //alert("Great- This is the right answer!!!!");
+    } else {
+        selectedBtn.classList.add("incorrect");
+        //alert("Sorry- Incorrect- Try again!!!!");
+        wrongAnswers++;
+    }
 }
-/* Create function when user clicks on the choices offered*/
-function selectChoice(event) {
-let selectedBtn = event.target;
-let isCorrect = selectedBtn.dataset.correct === "true";
-if (isCorrect) {
-    selectedBtn.classList.add("correct");
-    score++;
-    alert("Great- This is the right answer!!!!");
-} else {
-    selectedBtn.classList.add("incorrect");
-    alert("Sorry- Incorrect- Try again!!!!");
+
+function showscore() {
+    resetState();
+    quizH2Element.innerHTML = `You scored ${score} out of ${totalQuestions.length}!`;
+    scoreArea.innerHTML =`You got ${wrongAnswers} Wrong Answer(s),  you got ${correctAnswers}Correct Answer(s) `;
+    submitButton.innerHTML = "Play-Again";
+    submitButton.style.display = "block";
 }
-}
+
 /* Create function for Submit Button */
 function handleSubmitButton() {
-presentQuestionIndex++;
-if (presentQuestionIndex < questions.length) {
-    displayQuestion();
-} else {
-    showscore();
-}
+    presentQuestionIndex++;
+    if (presentQuestionIndex < totalQuestions.length) {
+        displayQuestion();
+    } else {
+        showscore();
+       // correctAnswers;
+       // wrongAnswers;
+    }
 
 }
 
 /* Create eventListener when user clicks submit button*/
 submitButton.addEventListener("click", () => {
-if (presentQuestionIndex <= questions.length) {
-    handleSubmitButton()
-} else {
-    startQuiz()
-}
+    if (presentQuestionIndex <= totalQuestions.length) {
+        handleSubmitButton()
+    } else {
+        initiateQuiz()
+    }
 });
+
+
 
 displayQuestion()
